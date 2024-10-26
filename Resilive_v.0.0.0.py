c72e2183e387ddbdf9fae3bae1d7eaf6bcef2622
homedesktop@2DGP <gunageroo@gmail.com>
2024-10-27 00:26:41 +0900

    뼈대 다시 기본부터 제작+오류발견
    내가 글로벌 월드를 안적었구나!
    __init__에 _ 가 2개구나!
    cilp을 안적음

--- Resilive_v.0.0.0.py
+++ Resilive_v.0.0.0.py
@@ -1,42 +1,55 @@
 from pico2d import*
 
-class Community:
-    image = None
-    
-    def _init_(self):
-        self.x, self.y = 30,40
-        self.frame = 0
-        if Community.image == None:
-            Community.image = load_image('community_idle-Sheet.png')
 
+class Lilly:
+
+    def __init__(self):
+        self.x, self.y = 400,90  #테스트를 위한 임시값
+
+        self.frame = 0
+        self.image = load_image("lilly_idle_Sheet.png")
+#       self.image = None
 
     def update(self):
-        self.frame = (self.frame + 1) % 7
-#        self.x += 5
+        self.frame = (self.frame+1) % 5
+
+    def handle_event(self, event):
+        pass
 
     def draw(self):
-        self.image.clip_draw(self.frame*128, 0, 128, 128, self.x, self.y)
-        pass
+        self.image.clip_draw(self.frame*128,0, 128,128, self.x,self.y)
+        
 
 
 
 ##
 def reset_world():
+    global world
     global running
     
     running = True
+    world = []
 
-    
+    lilly = Lilly()
+    world.append(lilly)
 
+    print("lilly append")
+
+    pass
 
 
 def update_world():
+    for objt in world:
+        objt.update()
+        print("objt")
     pass
 
 
 def render_world():
     clear_canvas()
+    #
     update_canvas()
+    print("renderworld")
 
 
 
@@ -44,13 +57,15 @@ def render_world():
 ##
 open_canvas()
 
-
 reset_world()
 
-
-
 while running:
+    
+    update_world()
+#    render_world()
+    delay(0.04)
     pass
+
 
 
 close_canvas()
