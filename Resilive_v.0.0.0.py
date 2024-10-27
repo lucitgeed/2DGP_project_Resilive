eb66c3f2ba956370b90433361a0be776d4f65919
homedesktop@2DGP <gunageroo@gmail.com>
2024-10-27 17:35:20 +0900

    오류수정,오류새로생김
    시험공부해야하니까 오늘은 여기까지

--- Resilive_v.0.0.0.py
+++ Resilive_v.0.0.0.py
@@ -1,14 +1,17 @@
+from random import random
+
 from pico2d import*
 
 
 class Lilly:
+    image = None
 
     def __init__(self):
         self.x, self.y = 50,90  #테스트를 위한 임시값
-
+        
         self.frame = 0
-        self.image = load_image("lilly_idle_Sheet.png")
-#       self.image = None
+        if Lilly.image == None:
+            Lilly.image = load_image("lilly_idle_Sheet.png")
 
     def update(self):
         self.frame = (self.frame+1) % 5
@@ -22,26 +25,69 @@ class Lilly:
 
 
 
+class Eyes:
+    pass
+
+
+
+
+
+
+class Community:
+    image = None
+
+    def __init__(self):
+        self.x, self.y = random.randint(50,750, 50), random.randint(200,650, 100)
+        self.frame= random.randint(0,7)
+        if Community.image == None:
+            Community.image = load_image("community_idle_Sheet.png")
+
+    def update(self):
+        self.frame = (self.frame+1) %7
+        self.x = self.x + random.randint(-10, 10)
+
+    def handle_event(self, event):
+        pass
+
+    def draw(self):
+        self.image.clip_draw(self.frame*128,0, 128,128, self.x, self.y, 70,70)
+        #임시위치
+        #랜덤값 추가로 설정할 것
+
+
+
+        
+
+
+
+
+
+
+
+
 ##
 def reset_world():
     global world
     global running
     
+    global lilly
+    global group
+    
     running = True
     world = []
 
     lilly = Lilly()
+#    community = Community()
+    group = [Community() for i in range(5)]
+
     world.append(lilly)
-
-#    print("appended")
-
+    world += group
     pass
 
 
 def update_world():
     for objt in world:
         objt.update()
-#        print("objt")
     pass
 
 
@@ -52,6 +98,22 @@ def render_world():
         objt.draw()
         
     update_canvas()
+    pass
+
+
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            lilly.handle_event(event)
+
+
 
 
 
@@ -65,7 +127,7 @@ while running:
     
     update_world()
     render_world()
-    delay(0.08)
+    delay(0.09)
     pass
 
 
--- test.py
+++ test.py
@@ -2,15 +2,18 @@ from pico2d import*
 
 open_canvas()
 
-lilly = load_image("lilly_idle_Sheet.png")
+community = load_image("community_idle_Sheet.png")
 
 frame = 0
 for x in range(0,100):
     clear_canvas()
-    lilly.clip_draw(frame*128,0, 128,128, 400,300)
+    community.clip_draw(frame*128,0, 128,128, 400,300) 
+    print("clipdraw")
     update_canvas()
-    frame = (frame+1) %5
-    delay(0.08)
+    print("update")
+    frame = (frame+1) %7
+    print("%7")
+    delay(0.1)
 
 close_canvas()
 
